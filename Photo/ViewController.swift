2b21d57c4a01ee006c456092d491394aa54a19ed
Taylor Maxwell <taylor@imaxwell.net>
2020-02-05 06:04:58 -0800

    Initial Commit

---
+++ Photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by Taylor Maxwell on 2/5/20.
+//  Copyright © 2020 Taylor Maxwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
